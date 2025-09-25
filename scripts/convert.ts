c3debac0406a005d2e05da99f873aaed8b931436
PadowYT2 <me@padow.ru>
2025-09-25 22:44:22 +0300

    proper types from i18next

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -1,12 +1,10 @@
 import Bun from 'bun';
+import { ResourceKey, ResourceLanguage } from 'i18next';
 
 const original = import.meta.glob('./**/*.ts', { base: '../src/translations', import: 'translations', eager: true });
-const translations = Object.entries(original).reduce(
-    (acc, [key, value]) => {
-        acc[key.replace('./', '').replace('.ts', '')] = value;
-        return acc;
-    },
-    {} as Record<string, unknown>,
-);
+const translations = Object.entries(original).reduce((acc, [key, value]) => {
+    acc[key.replace('./', '').replace('.ts', '')] = value as ResourceKey;
+    return acc;
+}, {} as ResourceLanguage);
 
 Bun.write('./languages/en_US.json', JSON.stringify(translations));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
+import { ResourceKey, ResourceLanguage } from 'i18next';
+
 const languages = import.meta.glob('./*.json', { base: '../languages', import: 'default', eager: true });
-const resources = Object.entries(languages).reduce(
-    (acc, [key, value]) => {
-        acc[key.replace('./', '').replace('.json', '')] = value;
-        return acc;
-    },
-    {} as Record<string, unknown>,
-);
+const resources = Object.entries(languages).reduce((acc, [key, value]) => {
+    acc[key.replace('./', '').replace('.json', '')] = value as ResourceKey;
+    return acc;
+}, {} as ResourceLanguage);
 
 export { resources };
