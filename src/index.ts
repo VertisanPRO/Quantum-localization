8600f3d8ce09d7c05d4205763933ae77ded2f243
PadowYT2 <me@padow.ru>
2025-09-25 22:55:42 +0300

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { ResourceKey, ResourceLanguage } from 'i18next';
+import { Resource, ResourceLanguage } from 'i18next';
 
 const languages = import.meta.glob('./*.json', { base: '../languages', import: 'default', eager: true });
-const resources = Object.entries(languages).reduce((acc, [key, value]) => {
-    acc[key.replace('./', '').replace('.json', '')] = value as ResourceKey;
+const resources = Object.entries(languages).reduce<Resource>((acc, [key, value]) => {
+    acc[key.replace('./', '').replace('.json', '')] = value as ResourceLanguage;
     return acc;
-}, {} as ResourceLanguage);
+}, {});
 
 export { resources };
